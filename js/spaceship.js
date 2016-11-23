1cf81e6fbeab13878fa02768f272b2ba80d46282
Valentina <vbhm@live.at>
2016-11-23 19:13:14 +0100

    small collision changes

--- js/spaceship.js
+++ js/spaceship.js
@@ -18,17 +18,38 @@ pro5.spaceship = (function(){
             ship.mesh.position.y = 50;
             ship.mesh.scale.set(3, 3, 3);
         });
+        
+        
 
     }
 
     //Collision
+    var lockedright, lockedleft, lockedup, lockeddown, collision;
+
+    lockeddown = lockedleft = lockedright = lockedup = collision = false;
+
+
+
+    //Update Spaceship
+    var keyboard = new THREEx.KeyboardState();
+    var a = new THREE.Vector2(0, 0);
+    var maxspeed = 0.7;
+    //var boostmaxspeed = 10;
+    var rotspeed = 0.1;
+    var acc = 0.03;
+    //var boostacc = 0.1;
+    var damping = 0.9;
+    //var boostdamping = 0.98;
+    var cameraY,
+        boundry;
+
     checkForCollision = function checkForCollision(){
 
         if(ship != undefined){
 
             // direction vectors
             var rays = [
-                new THREE.Vector3(0, 1, 0),
+                /*new THREE.Vector3(0, 1, 0),
                 new THREE.Vector3(0, 0, 1),
                 new THREE.Vector3(1, 0, 0),
                 new THREE.Vector3(0, 0, -1),
@@ -41,7 +62,16 @@ pro5.spaceship = (function(){
                 new THREE.Vector3(1, -1, -1),
                 new THREE.Vector3(1, -1, 1),
                 new THREE.Vector3(-1, -1, 1),
-                new THREE.Vector3(-1, -1, -1)
+                new THREE.Vector3(-1, -1, -1)*/
+
+                new THREE.Vector3(0, 1, 0),
+                new THREE.Vector3(1, 1, 0),
+                new THREE.Vector3(1, 0, 0),
+                new THREE.Vector3(1, -1, 0),
+                new THREE.Vector3(0, -1, 0),
+                new THREE.Vector3(-1, -1, 0),
+                new THREE.Vector3(-1, 0, 0),
+                new THREE.Vector3(-1, 1, 0),
             ];
 
 
@@ -54,28 +84,30 @@ pro5.spaceship = (function(){
                 var intersections = raycaster.intersectObjects(pro5.Planet.arrayPlanets);
 
 
-                if(intersections.length > 0 && intersections[0].distance <= 10){
-                    // handle collision...                    
-                    console.log(intersections[0].object.name);
-                }
+                if(intersections.length > 0 && intersections[0].distance <= 5){
+                    // handle collision...
+                    collision = true;
+                    if(vertexIndex === 1 || vertexIndex === 2 || vertexIndex === 3)
+                        lockedright = true;
+                    else if(vertexIndex === 5 || vertexIndex === 6 || vertexIndex === 7)
+                        lockedleft = true;
+                    break;
+;
+                } else {
+                    collision = false;
+                    lockedright = false;
+                    lockedleft = false;
+                }               
             }
+
+            console.log(collision);
         }
     }
 
-    //Update Spaceship
-    var keyboard = new THREEx.KeyboardState();
-    var a = new THREE.Vector2(0, 0);
-    var maxspeed = 1;
-    var boostmaxspeed = 10;
-    var rotspeed = 0.1;
-    var acc = 0.03;
-    var boostacc = 0.1;
-    var damping = 0.9;
-    var boostdamping = 0.98;
-    var cameraY,
-        boundry;
-
     updateShip = function updateShip(cameraY, boundry){
+
+        checkForCollision();
+
         if(keyboard.pressed("left")) {
             ship.mesh.rotation.z += rotspeed;
             a.rotateAround({x:0, y:0}, rotspeed);
@@ -89,7 +121,7 @@ pro5.spaceship = (function(){
                 a.y += boostacc * Math.cos(ship.mesh.rotation.z);
                 a.x += -boostacc * Math.sin(ship.mesh.rotation.z);
             }
-        } else*/ if(keyboard.pressed("up") ){
+        } else*/ if(keyboard.pressed("up")){
             /*if(a.length() > maxspeed){
                 a.y *= boostdamping;
                 a.x *= boostdamping;
