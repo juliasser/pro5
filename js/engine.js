60417deec07c3b60ab07518087f80c0e788122d6
Melanie Witzmann <meli-w@gmx.at>
2017-01-31 11:01:12 +0100

    first attempts for impressum

--- js/engine.js
+++ js/engine.js
@@ -52,6 +52,7 @@ pro5.engine = (function(){
         nextPage,
         prevPage,
         portalToPlanet,
+        showImpressum,
 
         // getters/setters
         getCamera,
@@ -502,6 +503,13 @@ pro5.engine = (function(){
         }
     }
 
+    showImpressum = function showImpressum() {
+        $("#impressum--overlay").show();
+        $("#impressum--overlay").animate({
+                opacity: 1},
+                2000);
+    }
+
     /*
 	*	### Getters/Setters ###
 	*/
@@ -778,6 +786,10 @@ pro5.engine = (function(){
 
         window.addEventListener( 'resize', onWindowResize, false );
         document.addEventListener( 'keydown', playIntroSequence, false);
+
+        $( document ).ready(function() {
+            document.getElementById('impressum').addEventListener('click', showImpressum, false);
+        });
 
         if(DEBUG){
             var axis = new THREE.AxisHelper(100);
