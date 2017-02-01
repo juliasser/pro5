a6850004bdc7251264416d173c91b98b980da0fe
Valentina <vbhm@live.at>
2017-02-01 14:24:33 +0100

    added marker

--- js/spaceship.js
+++ js/spaceship.js
@@ -253,12 +253,28 @@ pro5.spaceship = (function(){
             markerNr = 10;
         }
         
-        else if (markerNr != 11 && currentSunDistance > 5300000000 && currentSunDistance < 9000000000){
+        else if (markerNr != 11 && currentSunDistance > 5300000000 && currentSunDistance < 5900000000){
             pro5.engine.appendMarker('death-star');
-            pro5.engine.markerstorage[0].position.y = 5600;
+            pro5.engine.markerstorage[0].position.y = 5900;
             console.log("set 11");
             markerNr = 11;
         }
+        
+        else if (markerNr != 12 && currentSunDistance > 5900000000 && currentSunDistance < 6200000000){
+            pro5.engine.appendMarker('solar-system-edge');
+            pro5.engine.markerstorage[0].position.y = 6400;
+            console.log("set 12");
+            markerNr = 12;
+        }
+        
+        else if (markerNr != 13 && currentSunDistance > 6200000000 && currentSunDistance < 15000000000){
+            pro5.engine.appendMarker('time-left');
+            pro5.engine.markerstorage[0].position.y = 6700;
+            console.log("set 13");
+            markerNr = 13;
+        }
+        
+        
     }
 
     /*
